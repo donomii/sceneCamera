13cc9231b9c69eb3517cd43cc9f8ee12423c8e54
Jeremy Price <donomii@gmail.com>
2016-08-27 16:21:27 +0200

    Changes to work with 3D scenes

--- sceneCamera.go
+++ sceneCamera.go
@@ -7,20 +7,39 @@ type SceneCamera struct {
     Camera  mgl32.Mat4
     RotationMatrix mgl32.Mat4  
     PrevTime int64       //Device timestamp.  Does not hold correct time, is only useful for delta time
+    flipCam  bool       //Draw the mirror version of a scene
 }
 
 //New Creates a new sora and returns it.  The camera is positioned at 0.5 on the Y axis, and is looking directly down the axis in the -Y direction (i.e. it is looking at 0,0,0).
 func New () *SceneCamera {
+    return myinit()
+}
+
+func myinit () *SceneCamera {
     s := SceneCamera{}
-    s.Camera = mgl32.LookAt(0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+    s.Camera = mgl32.LookAt(0.0, 0.0, 0.8, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
     s.RotationMatrix = mgl32.Ident4()
     return &s
 }
 
+func (s *SceneCamera) FlipCam ()  {
+    //sMat := mgl32.Scale3D(1.0,-1.0,1.0)
+    //s.Camera = compose(sMat, s.Camera)
+    s.flipCam = !s.flipCam
+}
+
+
 //ViewMatrix returns the camera matrix, aka the "view" matrix, which forms the V part of the MVP matrix for 3D graphics.
 func (s *SceneCamera) ViewMatrix() mgl32.Mat4 {
-    return s.Camera     //Fixme
+    //sMat := mgl32.Scale3D(1.0,-1.0,1.0)
+    //return compose(sMat, s.Camera)     //Fixme
+    return s.Camera
 }
+
+func (s *SceneCamera) Reset() {
+    s = myinit()      //Fixme
+}
+
 
 //Sets the internal view matrix, replacing it with your own mgl32.Mat4
 func (s *SceneCamera) SetViewMatrix(newMatrix mgl32.Mat4) {
@@ -43,8 +62,8 @@ func (s *SceneCamera) ProcessEvent(e sensor.Event) {
                   s.PrevTime = e.Timestamp
                   scale := float32(360000000.0/float32(delta))   //Arbitrary scale, works for my phone, not sure if universal?
                   var sora mgl32.Vec3   //The real sora
-                  sora = mgl32.Vec3{float32(-e.Data[1])/scale, float32(e.Data[0])/scale,float32(-e.Data[2])/scale/float32(3.14)}
+                  sora = mgl32.Vec3{float32(-e.Data[1])/scale, float32(e.Data[0])/scale,float32(-e.Data[2])/scale/float32(3.14)/2.0}
                   s_norm := sora.Normalize()
                   rotMatrix := mgl32.HomogRotate3D(sora.Len(), s_norm)
-                  s.RotationMatrix = compose(rotMatrix, s.RotationMatrix)
+                  s.Camera = compose(rotMatrix, s.Camera)
 }
