0db69129fadc46970b9c4b101d78ad1f366534f5
Jeremy Price <donomii@gmail.com>
2023-04-08 03:00:50 +0200

    .

--- sceneCamera.go
+++ sceneCamera.go
@@ -167,22 +167,22 @@ func (c *Camera) moveFPSMode(direction int, amount float32) {
 
 	switch direction {
 	case 0: // Move forward
-		c.position = c.position.Add(forward.Mul(amount))
-
-	case 1: // Move backward
 		c.position = c.position.Sub(forward.Mul(amount))
 
-	case 2: // Strafe left
-		c.position = c.position.Sub(right.Mul(amount))
+	case 1: // Move backward
+		c.position = c.position.Add(forward.Mul(amount))
 
-	case 3: // Strafe right
+	case 2: // Strafe left
 		c.position = c.position.Add(right.Mul(amount))
 
+	case 3: // Strafe right
+		c.position = c.position.Sub(right.Mul(amount))
+
 	case 4: // Move up
-		c.position = c.position.Add(up.Mul(amount))
+		c.position = c.position.Sub(up.Mul(amount))
 
 	case 5: // Move down
-		c.position = c.position.Sub(up.Mul(amount))
+		c.position = c.position.Add(up.Mul(amount))
 
 	case 6: // Pitch up
 		c.Rotate(-amount, 0, 0)
